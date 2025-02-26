70d97339b941007a36b24a2007fd9dc7a13bc19c
allan.yen <iamallan0917@gmail.com>
2025-02-26 23:32:28 +0800

    20250226 1130 調整orderModoal

--- src/component/front/OrderModal.jsx
+++ src/component/front/OrderModal.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 /* eslint-disable react/prop-types */
 import { useRef, useEffect, useState, Fragment } from "react";
 import ReactLoading from "react-loading";
@@ -6,7 +7,7 @@ import { Modal } from "bootstrap";
 import { Modal as PicModal } from "../../component/common";
 const APIPath = import.meta.env.VITE_API_PATH;
 import { useToast } from "../../hook";
-import { orderDefaultValue } from "../../data/defaultValue";
+// import { Carts } from "../front";
 const OrderModal = (props) => {
   const { tempProduct, setIsProductModalOpen, isProductModalOpen } = props;
   const [modalProduct, setModalProduct] = useState(tempProduct);
@@ -23,21 +24,12 @@ const OrderModal = (props) => {
     const modalInstance = Modal.getInstance(productModalRef.current);
     modalInstance.show();
   };
-
-  const [toggleState, setToggleState] = useState({});
-  const handleToggleDetail = (key) => {
-    setToggleState((prev) => ({
-      ...prev,
-      [key]: !prev[key],
-    }));
-  };
   console.log("setModalProduct:", modalProduct);
   useEffect(() => {
     if (productModalRef.current) {
       const modalElement = productModalRef.current;
       new Modal(productModalRef.current, { backdrop: true });
       const handleClose = () => {
-        setToggleState({});
         setIsProductModalOpen(false);
       };
       modalElement.addEventListener("hidden.bs.modal", handleClose);
@@ -56,7 +48,7 @@ const OrderModal = (props) => {
   return (
     <>
       <div
-        id="productModal"
+        id="productModal modal-lg"
         className="modal fade"
         style={{ backgroundColor: "rgba(0,0,0,0.1)" }}
         ref={productModalRef}
@@ -64,7 +56,7 @@ const OrderModal = (props) => {
         <div className="modal-dialog modal-dialog-centered modal-xl">
           <div className="modal-content border-0 shadow">
             <div className="modal-header border-bottom">
-              {/* <h5 className="modal-title fs-4">訂單內容:{tempProduct.id}</h5> */}
+              <span className="modal-title">訂單內容</span>
               <button
                 type="button"
                 className="btn-close"
@@ -75,84 +67,69 @@ const OrderModal = (props) => {
             </div>
             <div className="modal-body">
               <div className="mb-3">
-                <span className="text-success fw-bold">訂購商品資料:</span>
+                {/* <span className="text-success fw-bold">訂購商品資料:</span> */}
                 {modalProduct !== undefined &&
                   Object.keys(modalProduct).length > 0 && (
-                    <div className="modal-body p-4">
-                      <div className="row g-4">
-                        <div className="col-md-12">
-                          <div className="mb-3">
-                            <ul className="list-group">
-                              <li
-                                className={`fw-bold ${
-                                  modalProduct.is_paid
-                                    ? "text-success"
-                                    : "text-danger "
-                                }`}
+                    <table className="table">
+                      <thead>
+                        <tr>
+                          <th scope="col" className="border-0 ps-0">
+                            商品
+                          </th>
+                          <th scope="col" className="border-0">
+                            數量
+                          </th>
+                          <th scope="col" className="border-0">
+                            總價
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {Object.entries(modalProduct.products).map(
+                          ([key, value]) => (
+                            <tr className="border-top" key={key}>
+                              <th
+                                scope="row"
+                                className="border-0 px-0 font-weight-normal py-4"
                               >
-                                {modalProduct.is_paid ? "已付款" : "未付款"}
-                              </li>
-                            </ul>
-                            {Object.entries(modalProduct.products).map(
-                              ([key, value], index) => {
-                                return (
-                                  <Fragment key={key}>
-                                    <div className="text-primary">
-                                      訂購商品{index + 1}:
-                                    </div>
-                                    <span
-                                      onClick={() => handleToggleDetail(key)}
-                                      className={`toggle-span ${
-                                        toggleState[key]
-                                          ? "text-secondary"
-                                          : "text-warning"
-                                      }`}
-                                      style={{
-                                        cursor: "pointer",
-                                        display: "inline-flex",
-                                        alignItems: "center",
-                                      }}
-                                    >
-                                      {toggleState[key]
-                                        ? "隱藏詳細訊息🡅"
-                                        : "顯示詳細訊息🡇"}
-                                    </span>
-                                    {toggleState[key] && (
-                                      <ul className="list-group">
-                                        <li>Order product list ID: {key}</li>
-                                        <li>
-                                          Product product ID:{" "}
-                                          {value.product?.id}
-                                        </li>
-                                        <li>
-                                          Product Title: {value.product?.title}
-                                        </li>
-                                        <li>
-                                          Product Category:{" "}
-                                          {value.product?.category}
-                                        </li>
-                                        <li>Product qty: {value.qty}</li>
-                                        <li>
-                                          Product Origin Price:{" "}
-                                          {value.product?.origin_price}
-                                        </li>
-                                        <li>
-                                          Product Price: {value.product?.price}
-                                        </li>
-                                        <li>Product Total: {value.total}</li>
-                                      </ul>
-                                    )}
-
-                                    <hr />
-                                  </Fragment>
-                                );
-                              }
-                            )}
-                            <p>總計:{modalProduct.total}</p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
+                                <img
+                                  src={value.product?.imageUrl}
+                                  alt={value.product?.title}
+                                  style={{
+                                    width: "72px",
+                                    height: "72px",
+                                    objectFit: "cover",
+                                  }}
+                                />
+                                <p className="mb-0 fw-bold ms-3 d-inline-block">
+                                  {value.product?.title}
+                                </p>
+                              </th>
+                              <td
+                                className="border-0 align-middle"
+                                style={{ maxWidth: "100px" }}
+                              >
+                                <div className="input-group pe-5">
+                                  <span
+                                    className="form-control border-0 text-center d-flex  justify-content-start my-auto shadow-none"
+                                    placeholder=""
+                                    aria-label="Example text with button addon"
+                                    aria-describedby="button-addon1"
+                                  >
+                                    {value.qty}
+                                  </span>
+                                </div>
+                              </td>
+                              <td className="border-0 align-middle">
+                                <p className="mb-0 ms-auto">
+                                  NT${value.total.toLocaleString()}
+                                </p>
+                              </td>
+                            </tr>
+                          )
+                        )}
+                      </tbody>
+                    </table>
                   )}
               </div>
               <div className="modal-footer border-top bg-light">
